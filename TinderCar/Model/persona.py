8776b7a731f01a20ce8a37d75ba313d9398d3ee6
Damian Rodriguez <damian.hr.rodriguez@gmail.com>
2022-11-07 11:02:08 -0300

    modifique la clase persona y cree la carpeta Js

--- TinderCar/Model/persona.py
+++ TinderCar/Model/persona.py
@@ -1,27 +1,13 @@
-class Persona:
-    nom_cli = ''
-    apell_cli = ''
-    direccion_cli = ''
-    dni_cli = ''
-    id_adm1 = ''
+class Persona():
+    #clase persona
 
-    #constructor de la clase
-    def __init__(self, nom_cli):
-        self.nom_cli = nom_cli
+    def __init__(self, nombre, apellido, direccion, telefono, dni, idAdm1):
+       self.nombre = nombre
+       self.apellido = apellido
+       self.direccion = direccion
+       self.telefono = telefono
+       self.dni = dni
+       self.idAdm1 = idAdm1
 
-    #setters
-    def setNom_cli(self,nom_cli):
-        self.nom_cli = nom_cli
-
-    def setApell_cli(self,apell_cli):
-        self.apell_cli = apell_cli
-
-    def setDireccion_cli(self, direccion_cli):
-        self.direccion_cli = direccion_cli
-
-    def setDni_cli(self,dni_cli):
-        self.dni_cli = dni_cli
     
-    def setId_adm1(self,id_adm1):
-        self.id_adm1 = id_adm1
     
